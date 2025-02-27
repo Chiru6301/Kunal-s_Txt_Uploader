21daa37dcb1b9130d4d11cc7008c52e5675a813d
Chiru6301 <chiruedizz@gmail.com>
2025-02-27 21:31:15 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -21,9 +21,9 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from bs4 import BeautifulSoup
 
 bot = Client("bot",
-             bot_token= "7018437002:AAG6WuPCt4Hb9VmPG5PmP0kusK3hyaMitJg", 
-             api_id= 27994649,
-             api_hash= "a667f9bf86b1e9af6ee9862fee3001e4")
+             bot_token= "7737490454:AAE5VVCe8R6xnvy6yFPp320i1JH_dZSDOqk", 
+             api_id= 28817205,
+             api_hash= "f319d02866bf7b83e4de31002f6ba8a3")
 
 
 #----------CONVERTOR-------------
