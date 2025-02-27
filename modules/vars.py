f69245fc22eef9bab7c97b99f4b6f4337d9ff362
Chiru6301 <chiruedizz@gmail.com>
2025-02-27 21:28:12 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "27994649"
-api_hash = "a667f9bf86b1e9af6ee9862fee3001e4"
-bot_token = "6767726971:AAG0kQTgbAXaqyd0kd1_tR0vdiAO21J48eQ"
+api_id = "28817205"
+api_hash = "f319d02866bf7b83e4de31002f6ba8a3"
+bot_token = "7737490454:AAE5VVCe8R6xnvy6yFPp320i1JH_dZSDOqk"
